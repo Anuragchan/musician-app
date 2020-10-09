de42569a9bd42cf9a55b636a19f49a2376610212
Anuragchan <48464897+Anuragchan@users.noreply.github.com>
2020-10-09 09:08:46 +0530

    Update musician.js

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: OMG it works!!");
+  res.status('200').send("Status: Role works?");
 });
 
 // retrieve all musicians from data store
